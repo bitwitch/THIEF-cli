52031be5762c253570e2639a76cfc524c3ed8405
colesayer <colesayershapiro@gmail.com>
2017-09-11 18:47:48 -0400

    seeded csv files

---
+++ db/migrate/20170911212402_create_solutions_table.rb
@@ -0,0 +1,8 @@
+class CreateSolutionsTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :solutions do |t|
+      t.integer :obstacle_id
+      t.integer :gadget_id
+    end
+  end
+end
